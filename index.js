877c9971c10bbea41bcef9a05286ad0b2af5ccfc
Blackk88 <108989350+Blackk88@users.noreply.github.com>
2022-08-25 15:52:08 +0300

    responsive design

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-document.querySelector("#carousel-left").addEventListener('click', () => {
-    console.log("clicked")
-})
+const workExamples = [
+    "image-slide-1.jpg",
+    "image-slide-2.jpg",
+    "image-slide-3.jpg",
+    "image-slide-4.jpg",
+    "image-slide-5.jpg"
+]
+
+let count = 2
+
+const workExamplesEl = document.querySelector(".carousel-image-wrapper")
+const examplesAmount = workExamples.length
 
 let test = document.querySelectorAll(".carousel-image-wrapper img")
 
-console.log(test.length)
+
+
+document.querySelector("#carousel-left").addEventListener('click', () => {
+    if (count === 1) {
+        count = examplesAmount
+    } else count--
+    
+    workExamplesEl.innerHTML = `
+        <img class="" src="./assets/images/image-slide-${count-1 === 0 ? examplesAmount : count-1}.jpg" alt="">
+        <img class="" src="./assets/images/image-slide-${count}.jpg" alt="">
+        <img class="" src="./assets/images/image-slide-${count+1 > examplesAmount ? examplesAmount-1 : count+1}.jpg" alt="">
+    `
+})
+document.querySelector("#carousel-right").addEventListener('click', () => {
+    if (count === examplesAmount) {
+        count = 1
+    } else count++
+    
+    workExamplesEl.innerHTML = `
+        <img class="" src="./assets/images/image-slide-${count-1 === 0 ? examplesAmount : count - 1}.jpg" alt="">
+        <img class="" src="./assets/images/image-slide-${count}.jpg" alt="">
+        <img class="" src="./assets/images/image-slide-${count+1 > examplesAmount ? examplesAmount - 1 : count+1}.jpg" alt="">
+    `
+})
